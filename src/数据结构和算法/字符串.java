19f8626ad483d008830255adc115fb70d61f4de5
dlm_global <dlmazz_global@163.com>
2019-01-12 14:04:58 +0800

    算法字符串向左平移，反转字符串

---
+++ src/数据结构和算法/字符串.java
@@ -0,0 +1,41 @@
+package 数据结构和算法;
+
+public class 字符串 {
+
+    public static void main(String[] args){
+        System.out.println(reverse("abcdefg"));
+        System.out.println(translateString("abcdefg"));
+
+    }
+
+
+    /**
+     * 反转字符串
+     * @param str
+     * @return
+     */
+    public static String reverse(String str){
+        if (str == null || str.equals("")){
+            return str;
+        }
+        return str.charAt(str.length() - 1) + reverse(str.substring(0,str.length() - 1));
+    }
+
+    /**
+     * 向左平移某段字符串
+     * @param str
+     * @return
+     */
+    public static String translateString(String str){
+
+        int n = (str.length() - 1) / 2;//左移n位
+        String str1 = reverse(str.substring(0,n + 1));
+        String str2 = reverse(str.substring(n + 1));
+        System.out.println(str1);
+        System.out.println(str2);
+        return reverse(str1 + str2);
+    }
+
+
+
+}
